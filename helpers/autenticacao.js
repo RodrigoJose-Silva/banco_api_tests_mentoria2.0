bdff3594d5df968623ef853817dee261e758acbd
Rodrigo Jose <digo.1903@hotmail.com>
2025-07-06 21:44:01 -0300

    add fixtures para bodies das requests

--- helpers/autenticacao.js
+++ helpers/autenticacao.js
@@ -1,13 +1,13 @@
 const request = require('supertest')
+const post_login = require('../fixtures/post_login.json')
 
 const obterToken = async (username, senha) => {
+    const bodyLogin = { ...post_login }
+    
     const responseLogin = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
-                .send({
-                    'username': username,
-                    'senha': senha
-                })
+                .send(bodyLogin)
             return token = responseLogin.body.token
 }
 
--- tests/transferencias.test.js
+++ tests/transferencias.test.js
@@ -2,6 +2,7 @@ const request = require('supertest')
 const { expect } = require('chai')
 require('dotenv').config()
 const { obterToken } = require('../helpers/autenticacao')
+const post_trasnferencias = require('../fixtures/post_trasnferencias.json')
 
 describe('Transfências', () => {
     describe('POST / Transferências', () => {
@@ -12,31 +13,26 @@ describe('Transfências', () => {
         })
         
         it('Deverá retornar 201 quando a transferência for feita com sucesso com um valor igual o maior que R$ 10 sem token', async ()=> { 
+            const bodyTransferencias = {...post_trasnferencias}
+
             const response = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    'contaOrigem': 1,
-                    'contaDestino': 2,
-                    'valor': 10,
-                    'token': ''
-                })
+                .send(bodyTransferencias)
             expect(response.statusCode).to.equal(201)
             expect(response.body.message).to.be.equal('Transferência realizada com sucesso.')
         })
 
         it('Deverá retornar 422 quando a transferência for feita com sucesso com um valor abaixo R$ 10 sem token', async ()=> { 
+            const bodyTransferencias = {...post_trasnferencias}
+            bodyTransferencias.valor= 9.99
+
             const response = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    'contaOrigem': 1,
-                    'contaDestino': 2,
-                    'valor': 9.99,
-                    'token': ''
-                })
+                .send(bodyTransferencias)
             expect(response.statusCode).to.equal(422)
             expect(response.body.error).to.be.equal('O valor da transferência deve ser maior ou igual a R$10,00.')
         })
